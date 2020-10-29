692cf55a29f9c135077e5ef4d791a272dd63b279
Cullen S <cullensmi@gmail.com>
2020-10-29 13:17:46 -0400

    First commit of day 1 of Hacker Rank challenge

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,23 +1,26 @@
 package com.csmithswim;
-
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+
         // Create a Scanner object to read input from stdin.
         Scanner scan = new Scanner(System.in);
 
         // Read a full line of input from stdin and save it to our variable, inputString.
+        Integer inputInteger = scan.nextInt();
+
+        Double inputDouble = scan.nextDouble();
+
         String inputString = scan.nextLine();
 
-        // Close the scanner object, because we've finished reading
-        // all of the input from stdin needed for this challenge.
-        scan.close();
-
-        // Print a string literal saying "Hello, World." to stdout.
-        System.out.println("Hello, World.");
-        System.out.println(inputString);
+        System.out.println(inputInteger+i);
+        System.out.println(inputDouble+2);
+        System.out.println(s+inputString);
 
         // TODO: Write a line of code here that prints the contents of inputString to stdout.
     }
