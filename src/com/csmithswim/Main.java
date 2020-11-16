8f52c231f970335ff910caadf62ec81015cd5bc4
Cullen S <cullensmi@gmail.com>
2020-11-16 16:09:00 -0500

    Solved operators algorithm.

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -24,7 +24,12 @@ public class Main {
     }
 
     static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        
+        double tip = (meal_cost * ((double)tip_percent/100));
+        double tax = (meal_cost * ((double)tax_percent/100));
+        double totalCost = meal_cost + tip + tax;
+        int bill = (int)Math.round(totalCost);
 
-
+        System.out.println(bill);
     }
 }
