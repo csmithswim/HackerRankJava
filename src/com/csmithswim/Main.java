1aa362c3fcfc5776098672864d09f260e2bc4220
Cullen S <cullensmi@gmail.com>
2020-11-16 16:22:19 -0500

    Solved algorithm

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -23,6 +23,12 @@ public class Main {
         else if (N%2 == 0 && N>=2 && N<=5){
             System.out.println("Not Weird");
         }
+        else if (N%2 == 0 && N>=6 && N<=20){
+            System.out.println("Weird");
+        }
+        else if (N%2 == 0 && N>20){
+            System.out.println("Not Weird");
+        }
 
         scanner.close();
     }
