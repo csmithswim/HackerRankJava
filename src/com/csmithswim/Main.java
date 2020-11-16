c4d910b08c6263867650c68d0f8329aeb28b9630
Cullen S <cullensmi@gmail.com>
2020-11-16 16:19:49 -0500

    First commit of intro to conditional statements

--- src/com/csmithswim/Main.java
+++ src/com/csmithswim/Main.java
@@ -1,35 +1,30 @@
 package com.csmithswim;
-import java.util.Scanner;
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
 
 //Calculate the final bill one pays when given a tax, base price of food and a calculated tip.
 
 public class Main {
+    private static final Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args) {
-          // Create a Scanner object to read input from stdin.
-        Scanner scanner = new Scanner(System.in);
-
-        double meal_cost = scanner.nextDouble();
+        int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int tip_percent = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        int tax_percent = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-
-        solve(meal_cost, tip_percent, tax_percent);
+        if ((N-1)%2 == 0){
+            System.out.println("Weird");
+        }
+        else if (N%2 == 0 && N>=2 && N<=5){
+            System.out.println("Not Weird");
+        }
 
         scanner.close();
     }
 
-    static void solve(double meal_cost, int tip_percent, int tax_percent) {
-        
-        double tip = (meal_cost * ((double)tip_percent/100));
-        double tax = (meal_cost * ((double)tax_percent/100));
-        double totalCost = meal_cost + tip + tax;
-        int bill = (int)Math.round(totalCost);
-
-        System.out.println(bill);
-    }
 }
